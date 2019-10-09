0907f3155eed551f84b351d55f3bd216d399459c
mpppk <niboshiporipori@gmail.com>
2019-10-09 21:01:42 +0900

    Fix test

--- messagen/internal/repository_test.go
+++ messagen/internal/repository_test.go
@@ -363,7 +363,7 @@ func TestRandomTemplatePicker(t *testing.T) {
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, err := RandomTemplatePicker(tt.args.def, tt.args.aliasName, tt.args.alias, tt.args.state)
+			got, err := RandomTemplatePicker(tt.args.def, tt.args.state)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("RandomTemplatePicker() error = %v, wantErr %v", err, tt.wantErr)
 				return
