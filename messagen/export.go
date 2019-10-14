b19c92db9fa8bcd4173105aa12abca2257ac506b
niboshi <niboshiporipori@gmail.com>
2019-10-14 11:46:17 +0900

    Implement IrohaTemplateValidator (#22)
    * Implement IrohaTemplateValidator
    
    * Update pokemon list

--- messagen/export.go
+++ messagen/export.go
@@ -4,6 +4,7 @@ import "github.com/mpppk/messagen/messagen/internal"
 
 type DefinitionWithAlias = internal.DefinitionWithAlias
 type State = internal.State
+type Template = internal.Template
 type Templates = internal.Templates
 type TemplatePicker = internal.TemplatePicker
 type DefinitionPicker = internal.DefinitionPicker
