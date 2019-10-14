6cd674b408b310ada7d2c96ceb16a9116d9c9772
mpppk <niboshiporipori@gmail.com>
2019-10-14 23:08:05 +0900

    Add state flag to run command

--- internal/option/run.go
+++ internal/option/run.go
@@ -1,27 +1,52 @@
 package option
 
 import (
+	"fmt"
+	"strings"
+
 	"github.com/spf13/viper"
 	"golang.org/x/xerrors"
 )
 
 type RunCmdConfig struct {
-	FilePath string
-	RootType string
-	Num      int
+	FilePath     string
+	RootType     string
+	Num          int
+	InitialState map[string]string
 }
 
 func NewRunCmdConfigFromViper() (*RunCmdConfig, error) {
 	rawConfig, err := newRunCmdRawConfig()
-	return newRunCmdConfigFromRawConfig(rawConfig), err
+	if err != nil {
+		return nil, err
+	}
+	return newRunCmdConfigFromRawConfig(rawConfig)
 }
 
-func newRunCmdConfigFromRawConfig(rawConfig *RunCmdRawConfig) *RunCmdConfig {
-	return &RunCmdConfig{
-		FilePath: rawConfig.File,
-		RootType: rawConfig.Root,
-		Num:      rawConfig.Num,
+func newRunCmdConfigFromRawConfig(rawConfig *RunCmdRawConfig) (*RunCmdConfig, error) {
+	state, err := parseKVStr(rawConfig.State)
+	if err != nil {
+		return nil, err
 	}
+	return &RunCmdConfig{
+		FilePath:     rawConfig.File,
+		RootType:     rawConfig.Root,
+		Num:          rawConfig.Num,
+		InitialState: state,
+	}, nil
+}
+
+func parseKVStr(kvListStr string) (map[string]string, error) {
+	m := map[string]string{}
+	kvList := strings.Split(kvListStr, ",")
+	for _, kv := range kvList {
+		keyAndValue := strings.Split(kv, "=")
+		if len(keyAndValue) != 2 {
+			return nil, fmt.Errorf("invalid key and value string. %s", kvListStr)
+		}
+		m[keyAndValue[0]] = keyAndValue[1]
+	}
+	return m, nil
 }
 
 func newRunCmdRawConfig() (*RunCmdRawConfig, error) {
@@ -34,7 +59,8 @@ func newRunCmdRawConfig() (*RunCmdRawConfig, error) {
 }
 
 type RunCmdRawConfig struct {
-	File string
-	Root string
-	Num  int
+	File  string
+	Root  string
+	Num   int
+	State string // TODO: viper cannot parse map[string]string correctly. See https://github.com/spf13/viper/issues/608
 }
